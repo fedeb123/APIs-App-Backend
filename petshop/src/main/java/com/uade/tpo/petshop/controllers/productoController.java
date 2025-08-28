fde4eb49b579b700e6a0bcd11218eaccd9d8350e
francolautaro2 <francooro322@gmail.com>
2025-08-28 18:23:36 -0300

    feat: se creo el service y su implementacion

--- petshop/src/main/java/com/uade/tpo/petshop/controllers/productoController.java
+++ petshop/src/main/java/com/uade/tpo/petshop/controllers/productoController.java
@@ -1,6 +1,39 @@
 package com.uade.tpo.petshop.controllers;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.UUID;
+import java.util.Optional;
+
+import com.uade.tpo.petshop.entity.Producto;
+import com.uade.tpo.petshop.service.productoService;
+
+
+@RestController
+@RequestMapping("productos")
 public class productoController {
     // TODO : Hacer los controllers :)
+    @Autowired
+    private productoService productoService;
+
+    @GetMapping
+    public ResponseEntity<Page<Producto>> getAllProductos() {
+        Page<Producto> productos = productoService.getAllProductos();
+        return ResponseEntity.ok(productos);
+    }
+
+    @GetMapping("/{productoId}")
+    public ResponseEntity<Producto> getProductoById(@PathVariable UUID productoId) {
+        Optional<Producto> producto = productoService.getProductoById(productoId);
+        return producto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+    
+    
+
 }
