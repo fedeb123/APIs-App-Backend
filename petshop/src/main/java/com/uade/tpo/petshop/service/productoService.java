62fe169e3488d1d419df2943a9c6f0bbb6fc3b61
Franco <69493845+francolautaro2@users.noreply.github.com>
2025-08-28 18:24:46 -0300

    Feature/producto service controller (#2)
    * se agregaron dependencias
    
    * Se agregaron dependencias
    
    * feat: se creo el service y su implementacion

--- petshop/src/main/java/com/uade/tpo/petshop/service/productoService.java
+++ petshop/src/main/java/com/uade/tpo/petshop/service/productoService.java
@@ -1,5 +1,15 @@
 package com.uade.tpo.petshop.service;
 
-public class productoService {
-    // TODO: hacer los services :)
+import com.uade.tpo.petshop.entity.Producto;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import com.uade.tpo.petshop.entity.Categoria;
+import org.springframework.data.domain.Page;
+
+public interface productoService {
+    // metodo que nos trae todas las productos
+    public Page<Producto> getAllProductos();
+    public Optional<Producto> getProductoById(UUID id);
+    public Producto createProducto(Producto producto);
 }
