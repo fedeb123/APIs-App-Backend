9c9175f20f8a30341be5a654bbfefc8c6acf2053
fedeb123 <federicoab@gmail.com>
2025-08-29 15:02:21 -0300

    agregado de rol repository

---
+++ petshop/src/main/java/com/uade/tpo/petshop/repositories/interfaces/IRolRepository.java
@@ -0,0 +1,30 @@
+package com.uade.tpo.petshop.repositories.interfaces;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.uade.tpo.petshop.entity.Rol;
+import com.uade.tpo.petshop.entity.enums.RolEnum;
+
+
+/*
+ * Al extender JPA ya se estan incorporando metodos CRUD
+ * por ejemplo findById, save, etc
+ * Al querer poner un metodo personalizado, agregarlo de la forma:
+ * --
+ * @Query(value = "Poner la Query") 
+ * TipoDeDatoADevolver funcion parametros();
+ * --
+ * @fedeb123
+ */
+
+@Repository
+public interface IRolRepository extends JpaRepository<Rol, Long> {
+    
+    @Query(value = "SELECT R FROM ROL R WHERE ROL.nombre =?1")
+    List<Rol> findByName(RolEnum name);
+
+}
