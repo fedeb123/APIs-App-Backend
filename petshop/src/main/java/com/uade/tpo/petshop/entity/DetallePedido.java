aa1d3b149ea794d1ca2ce810a9d2201796b4b887
Jordii05 <jordicl2018@gmail.com>
2025-08-29 00:44:58 -0300

    Feature/estructura inicial (#4)
    * Feat: se creó la estructura inicial del proyecto
    
    * Modelo refactorizado

--- petshop/src/main/java/com/uade/tpo/petshop/entity/DetallePedido.java
+++ petshop/src/main/java/com/uade/tpo/petshop/entity/DetallePedido.java
@@ -1,17 +1,42 @@
 package com.uade.tpo.petshop.entity;
-import java.util.UUID;
-
-import lombok.Builder;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
 
 @Data
-@Builder
+@Entity
 public class DetallePedido {
-    private UUID id;
+    public DetallePedido() {
+    }
+
+    public DetallePedido(Pedido pedido, Producto producto, int cantidad, double precioUnitario) {
+        this.pedido = pedido;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne()
+    @JoinColumn(name="pedido_id", nullable=false)
     private Pedido pedido;
-    private Producto producto;
+    
+    @Column
     private int cantidad;
+    
+    @Column
     private double precioUnitario;
 
-    
+    @ManyToOne()
+    @JoinColumn(name="producto_id", nullable=false)
+    private Producto producto;
+
 }
--- petshop/src/main/java/com/uade/tpo/petshop/entity/Producto.java
+++ petshop/src/main/java/com/uade/tpo/petshop/entity/Producto.java
@@ -1,21 +1,53 @@
 package com.uade.tpo.petshop.entity;
-import java.util.UUID;
 
-import lombok.Builder;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 @Data
-@Builder
+@Entity
 public class Producto {
 
-    private UUID id;
+    public Producto() {
+    }
 
+    public Producto(String nombre, String descripcion, double precio, int stock, Categoria categoria) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.stock = stock;
+        this.categoria = categoria;
+    }
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable=false, unique=true)
     private String nombre;
 
+
+    @Column
     private String descripcion;
 
+    @Column
     private double precio;
 
+    @Column
     private int stock;
 
+    @ManyToOne()
+    @JoinColumn(name="categoria_id", nullable=false)
     private Categoria categoria;
+
+    @OneToMany(mappedBy="producto")
+    List<DetallePedido> detallePedidos;
+
 }
