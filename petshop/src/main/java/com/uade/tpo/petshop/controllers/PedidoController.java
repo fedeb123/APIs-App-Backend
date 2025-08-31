31991e1b2b96617f6fed12ab920ed067890a574e
fedeb123 <federicoab@gmail.com>
2025-08-31 20:16:58 -0300

    missing @autowired dependencies fix

--- petshop/src/main/java/com/uade/tpo/petshop/controllers/PedidoController.java
+++ petshop/src/main/java/com/uade/tpo/petshop/controllers/PedidoController.java
@@ -1,16 +1,25 @@
 package com.uade.tpo.petshop.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.uade.tpo.petshop.entity.Pedido;
 import com.uade.tpo.petshop.service.impl.PedidoServiceImpl;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/pedidos")
 public class PedidoController {
-
+    @Autowired
     private final PedidoServiceImpl pedidoService;
 
     public PedidoController(PedidoServiceImpl pedidoService) {
