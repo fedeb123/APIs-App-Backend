98857dab7e37ebe9bb1ebc935977d58d9a683158
fedeb123 <federicoab@gmail.com>
2025-09-01 12:36:56 -0300

    creacion de Usuario controller

---
+++ petshop/src/main/java/com/uade/tpo/petshop/controllers/UsuarioController.java
@@ -0,0 +1,57 @@
+package com.uade.tpo.petshop.controllers;
+
+import java.net.URI;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.uade.tpo.petshop.entity.Usuario;
+import com.uade.tpo.petshop.entity.dtos.UsuarioDTO;
+import com.uade.tpo.petshop.entity.exceptions.UsuarioDuplicateException;
+import com.uade.tpo.petshop.service.UsuarioService;
+
+@RestController
+@RequestMapping("api/usuarios")
+public class UsuarioController {
+	@Autowired
+	private final UsuarioService usuarioService;
+
+	public UsuarioController(UsuarioService usuarioService){
+		this.usuarioService = usuarioService;
+	}
+
+	@GetMapping
+	public ResponseEntity<Page<Usuario>> getAllUsuarios(@RequestParam(required = false) Integer page,@RequestParam(required = false) Integer size) {
+		if (page == null && size == null){
+			return ResponseEntity.ok(usuarioService.getAllUsuarios(PageRequest.of(0, Integer.MAX_VALUE)));
+		} else {
+			return ResponseEntity.ok(usuarioService.getAllUsuarios(PageRequest.of(page, size)));
+		}
+	}
+
+	@GetMapping("/{usuarioId}")
+	public ResponseEntity<Usuario> getUsuarioById(@PathVariable Long usuarioId) {
+		Optional<Usuario> usuario = usuarioService.getUsuarioById(usuarioId);
+		if (usuario.isPresent()){
+			return ResponseEntity.ok(usuario.get());
+		}
+		return ResponseEntity.notFound().build();
+	}
+
+	@PostMapping
+	public ResponseEntity<Object> createUsuario(@RequestBody UsuarioDTO usuario)
+			throws UsuarioDuplicateException {
+		Usuario nuevoUsuario = usuarioService.createUsuario(usuario);
+		return ResponseEntity.created(URI.create("/usuarios/" + nuevoUsuario.getId())).body(nuevoUsuario);
+	}    
+}
