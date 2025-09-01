e33b562ab39faf31ae2d86b1dee4f0b2250626c6
francolautaro2 <francooro322@gmail.com>
2025-09-01 15:34:00 -0300

    feat: se agrega el repository de producto

---
+++ petshop/src/main/java/com/uade/tpo/petshop/repositories/interfaces/IProductoRepository.java
@@ -0,0 +1,24 @@
+package com.uade.tpo.petshop.repositories.interfaces;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.uade.tpo.petshop.entity.Producto;
+import java.util.UUID;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, UUID> {
+    
+    @Query(value="SELECT p FROM Producto p WHERE p.nombre LIKE %:nombre%")
+    List<Producto> findByNombre(String nombre);
+    
+    @Query(value="SELECT p FROM Producto p WHERE p.id = :id")
+    Producto findProductoById(UUID id);
+
+    @Query(value="SELECT p FROM Producto p")
+    Page<Producto> findAllProductos();
+
+}
