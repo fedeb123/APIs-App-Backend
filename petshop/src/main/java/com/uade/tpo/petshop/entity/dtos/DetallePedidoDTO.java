1f6c914e3d8a12fc85a4bb3f5a210272ecb46dfe
fedeb123 <federicoab@gmail.com>
2025-09-02 16:53:17 -0300

    Arreglos en DTOs

--- petshop/src/main/java/com/uade/tpo/petshop/entity/dtos/DetallePedidoDTO.java
+++ petshop/src/main/java/com/uade/tpo/petshop/entity/dtos/DetallePedidoDTO.java
@@ -8,15 +8,15 @@ public class DetallePedidoDTO {
 	private Long id;
 	private int cantidad;
 	private double precioSubtotal;
-	private Long productoId;
-	private Long pedidoId;
+	private ProductoDTO producto;
+	private PedidoDTO pedido;
 
-	public DetallePedidoDTO(Long id, int cantidad, double precioSubtotal, Long productoId, Long pedidoId) {
+	public DetallePedidoDTO(Long id, int cantidad, double precioSubtotal, ProductoDTO producto, PedidoDTO pedido) {
 		this.id = id;
 		this.cantidad = cantidad;
 		this.precioSubtotal = precioSubtotal;
-		this.productoId = productoId;
-		this.pedidoId = pedidoId;
+		this.producto = producto;
+		this.pedido = pedido;
 	}
 
     public DetallePedidoDTO(){
--- petshop/src/main/java/com/uade/tpo/petshop/entity/dtos/FacturaDTO.java
+++ petshop/src/main/java/com/uade/tpo/petshop/entity/dtos/FacturaDTO.java
@@ -1,5 +1,32 @@
 package com.uade.tpo.petshop.entity.dtos;
 
+import java.util.Date;
+
+import com.uade.tpo.petshop.entity.enums.MetodoDePagoEnum;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.Data;
+
+@Data
 public class FacturaDTO {
     
+    private Long id;
+    private Date fechaEmision;
+    private double total;
+    private PedidoDTO pedido;
+    @Enumerated(EnumType.STRING)
+    private MetodoDePagoEnum metodoDePago;    
+    
+    public FacturaDTO(){
+
+    }
+
+    public FacturaDTO(Long id, Date fechaEmision, double total, PedidoDTO pedido, MetodoDePagoEnum metodoDePago){
+        this.id = id;
+        this.fechaEmision = fechaEmision;
+        this.total = total;
+        this.pedido = pedido;
+        this.metodoDePago = metodoDePago;
+    }
 }
