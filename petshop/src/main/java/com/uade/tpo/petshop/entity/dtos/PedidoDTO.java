b915e86689effe700d71f84faf601b292910c85f
fedeb123 <federicoab@gmail.com>
2025-09-02 16:53:17 -0300

    Arreglos en DTOs

--- petshop/src/main/java/com/uade/tpo/petshop/entity/dtos/PedidoDTO.java
+++ petshop/src/main/java/com/uade/tpo/petshop/entity/dtos/PedidoDTO.java
@@ -3,6 +3,8 @@ package com.uade.tpo.petshop.entity.dtos;
 import java.util.Date;
 import java.util.List;
 
+import com.uade.tpo.petshop.entity.enums.EstadoEnum;
+
 import lombok.Data;
 
 @Data
@@ -11,12 +13,12 @@ public class PedidoDTO {
     private Long id;
     UsuarioDTO cliente;
     private Date fecha;
-    private EstadoDTO estado;
+    private EstadoEnum estado;
     private double precioTotal;
     private List<DetallePedidoDTO> detalles;
     private FacturaDTO factura;
 
-    public PedidoDTO(Long id, UsuarioDTO cliente, Date fecha, EstadoDTO estado, double precioTotal, List<DetallePedidoDTO> detalles, FacturaDTO factura) {
+    public PedidoDTO(Long id, UsuarioDTO cliente, Date fecha, EstadoEnum estado, double precioTotal, List<DetallePedidoDTO> detalles, FacturaDTO factura) {
         this.id = id;
         this.cliente = cliente;
         this.fecha = fecha;
