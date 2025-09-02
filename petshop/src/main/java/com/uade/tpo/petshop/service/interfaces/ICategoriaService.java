d7dc3fcf037facac8547b98862203c96cc178484
fedeb123 <federicoab@gmail.com>
2025-09-02 18:24:50 -0300

    interfaces fix

--- petshop/src/main/java/com/uade/tpo/petshop/service/interfaces/ICategoriaService.java
+++ petshop/src/main/java/com/uade/tpo/petshop/service/interfaces/ICategoriaService.java
@@ -6,12 +6,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
 import com.uade.tpo.petshop.entity.Categoria;
-import com.uade.tpo.petshop.entity.dto.CategoriaDTO;
+import com.uade.tpo.petshop.entity.dtos.CategoriaDTO;
+import com.uade.tpo.petshop.entity.exceptions.CategoriaDuplicateException;
 
 public interface ICategoriaService {
     public Optional<Categoria> getCategoriaById(Long id);
     public Page<Categoria> getAllCategorias(PageRequest pageRequest);
-    public Categoria createCategoria(CategoriaDTO categoria) throws Exception;
+    public Categoria createCategoria(CategoriaDTO categoria) throws CategoriaDuplicateException;
     public Optional<Categoria> getCategoriaByNombre(String nombreCategoria);
     public void deleteCategoriaById(Long id);
 }
--- petshop/src/main/java/com/uade/tpo/petshop/service/interfaces/IProductoService.java
+++ petshop/src/main/java/com/uade/tpo/petshop/service/interfaces/IProductoService.java
@@ -1,17 +1,17 @@
 package com.uade.tpo.petshop.service.interfaces;
 
 import java.util.Optional;
-import java.lang.Long;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
-import com.uade.tpo.petshop.entity.dtos.ProductoDTO;
 import com.uade.tpo.petshop.entity.Producto;
+import com.uade.tpo.petshop.entity.dtos.ProductoDTO;
+import com.uade.tpo.petshop.entity.exceptions.MissingCategoriaException;
+import com.uade.tpo.petshop.entity.exceptions.ProductoDuplicateException;
 
 public interface IProductoService {
     public Page<Producto> getAllProductos(PageRequest pageable);
     public Optional<Producto> getProductoById(Long id);
-    public Producto createProducto(ProductoDTO producto);
-
+    public Producto createProducto(ProductoDTO producto) throws MissingCategoriaException, ProductoDuplicateException;
 }
