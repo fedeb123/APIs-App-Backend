966a407fbe81cffba8d4f7978c009301ca387585
fedeb123 <federicoab@gmail.com>
2025-09-02 18:40:34 -0300

    adicion de un manejador de excepciones genericas

---
+++ petshop/src/main/java/com/uade/tpo/petshop/entity/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.uade.tpo.petshop.entity.exceptions;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/*
+ * Esto atajaria cualquier excepcion generica 
+ * no especificada
+ */
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> manejarExcepcionGenerica(Exception error){
+        return ResponseEntity.status(500).body("error interno: " + error);
+    }
+    
+}
