98158c8ce120307e764816a8be142782596a5c72
fedeb123 <federicoab@gmail.com>
2025-09-11 16:08:31 -0300

    creadas las rutas protegidas

--- petshop/src/main/java/com/uade/tpo/petshop/controllers/config/SecurityConfig.java
+++ petshop/src/main/java/com/uade/tpo/petshop/controllers/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.uade.tpo.petshop.controllers.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -27,12 +28,33 @@ public class SecurityConfig {
         public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
                 http
                                 .csrf(AbstractHttpConfigurer::disable)
-                                .authorizeHttpRequests(req -> req.requestMatchers("/api/v1/auth/**").permitAll()
+                                .authorizeHttpRequests(req -> req
+                                                // Registro, Error y Docs publicos.
+                                                .requestMatchers("/api/v1/auth/**").permitAll()
                                                 .requestMatchers("/error/**").permitAll()
                                                 .requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll()
-                                                .requestMatchers("/categorias/**").hasAnyAuthority(RolEnum.CLIENTE.name())
-                                                .anyRequest()
-                                                .authenticated())
+
+                                                //Acciones Administrativas (@ADMIN)
+                                                .requestMatchers("/api/usuarios/**").hasAnyAuthority(RolEnum.ADMIN.name())
+                                                .requestMatchers("/api/roles/**").hasAnyAuthority(RolEnum.ADMIN.name())
+
+                                                .requestMatchers(HttpMethod.POST, "/api/categorias/**").hasAnyAuthority(RolEnum.ADMIN.name())
+                                                .requestMatchers(HttpMethod.PUT, "/api/categorias/**").hasAnyAuthority(RolEnum.ADMIN.name())                                                .requestMatchers(HttpMethod.PUT, "/api/productos/**").hasAnyAuthority(RolEnum.ADMIN.name())
+                                                .requestMatchers(HttpMethod.GET, "/api/pedidos/**").hasAnyAuthority(RolEnum.ADMIN.name())
+                                                .requestMatchers(HttpMethod.POST, "/api/pedidos/agregarFactura/**").hasAnyAuthority(RolEnum.ADMIN.name())
+                                                .requestMatchers(HttpMethod.DELETE, "/api/detalle-pedidos").authenticated()
+
+                                                //Acciones tanto para Admin como para Cliente
+                                                .requestMatchers(HttpMethod.GET, "/api/productos/**").authenticated()
+                                                .requestMatchers(HttpMethod.POST, "/api/pedidos/**").authenticated()
+                                                .requestMatchers(HttpMethod.PUT, "/api/pedidos/**").authenticated()
+                                                .requestMatchers(HttpMethod.GET, "/api/categorias/**").authenticated()
+                                                .requestMatchers(HttpMethod.POST, "/api/detalle-pedidos").authenticated()
+                                                .requestMatchers(HttpMethod.PUT, "/api/detalle-pedidos").authenticated()                                                .requestMatchers(HttpMethod.GET, "/api/detalle-pedidos").authenticated()                                                
+                                                
+
+
+                                                .anyRequest().authenticated())
                                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                                 .authenticationProvider(authenticationProvider)
                                 .addFilterBefore(filtroJwt, UsernamePasswordAuthenticationFilter.class);
