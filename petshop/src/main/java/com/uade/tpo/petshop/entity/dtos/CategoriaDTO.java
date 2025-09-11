43e496474320a76c8f9145b156dff5c335ec58b7
fedeb123 <federicoab@gmail.com>
2025-09-11 18:17:30 -0300

    Categoria DTO Fix

--- petshop/src/main/java/com/uade/tpo/petshop/entity/dtos/CategoriaDTO.java
+++ petshop/src/main/java/com/uade/tpo/petshop/entity/dtos/CategoriaDTO.java
@@ -9,13 +9,13 @@ public class CategoriaDTO {
     private Long id;
     private String nombreCategoria;
     private String descripcion;
-    private List<ProductoDTO> productos;
 
-    public CategoriaDTO(Long id, String nombreCategoria, String descripcion, List<ProductoDTO> productos) {
+
+    public CategoriaDTO(Long id, String nombreCategoria, String descripcion) {
         this.id = id;
         this.nombreCategoria = nombreCategoria;
         this.descripcion = descripcion;
-        this.productos = productos;
+
     }
 
     public CategoriaDTO() {
