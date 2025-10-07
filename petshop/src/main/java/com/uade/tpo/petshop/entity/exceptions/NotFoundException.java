23f3853baf4308f8db132c19b8c150db86cd3199
Jordii05 <jordicl2018@gmail.com>
2025-10-07 13:58:56 -0300

    excepcion de si se encuentra el id del pedido a modificar

--- petshop/src/main/java/com/uade/tpo/petshop/entity/exceptions/NotFoundException.java
+++ petshop/src/main/java/com/uade/tpo/petshop/entity/exceptions/NotFoundException.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 // Para cuando no se encuentra el detalle pedido
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "DetallePedido no encontrado")
-public class NotFoundException {
+public class NotFoundException extends Exception {
     
 }
