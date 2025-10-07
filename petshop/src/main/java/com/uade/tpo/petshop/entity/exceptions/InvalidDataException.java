1abdfef316af03fc797f2f52a58a155628e960d7
Jordii05 <jordicl2018@gmail.com>
2025-10-07 13:58:27 -0300

    excepcion de cantidad a modificar en un pedido

--- petshop/src/main/java/com/uade/tpo/petshop/entity/exceptions/InvalidDataException.java
+++ petshop/src/main/java/com/uade/tpo/petshop/entity/exceptions/InvalidDataException.java
@@ -5,6 +5,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 // Para cuando los datos del detalle son inválidos (cantidad negativa, excede stock, etc.)
 @ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Datos inválidos")
-public class InvalidDataException {
+public class InvalidDataException extends Exception {
     
 }
