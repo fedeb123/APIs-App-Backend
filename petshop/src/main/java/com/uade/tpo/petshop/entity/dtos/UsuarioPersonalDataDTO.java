91cdd8625e2fceb3d635cc659659b38037b513c0
Jordii05 <jordicl2018@gmail.com>
2025-10-16 00:37:58 -0300

    nueva forma de acceder a datos personales de usuario

---
+++ petshop/src/main/java/com/uade/tpo/petshop/entity/dtos/UsuarioPersonalDataDTO.java
@@ -0,0 +1,23 @@
+package com.uade.tpo.petshop.entity.dtos;
+import lombok.Data;
+
+@Data
+public class UsuarioPersonalDataDTO {
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+    private String direccion;
+
+    public UsuarioPersonalDataDTO (Long id, String nombre, String apellido, String telefono, String email, String direccion){
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+        this.direccion = direccion;
+    }
+
+    public UsuarioPersonalDataDTO(){}
+}
