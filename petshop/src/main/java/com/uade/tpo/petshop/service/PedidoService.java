6c4b491a02a5dc74636e7e43be83bba0d9902dc0
Franco <69493845+francolautaro2@users.noreply.github.com>
2025-11-27 13:30:39 -0300

    fix pedidos vacios (#38)
    * se arreglo que cuando un usuario no tenga ningun pedido asociado devuelva una lista vacia y no un error
    
    * se sacaron comentarios

--- petshop/src/main/java/com/uade/tpo/petshop/service/PedidoService.java
+++ petshop/src/main/java/com/uade/tpo/petshop/service/PedidoService.java
@@ -151,15 +151,11 @@ public class PedidoService implements IPedidoService {
     }
 
     @Override
-    public List<Pedido> getPedidosFromUsuario(String email) throws MissingUserException, MissingPedidoException {
-        Usuario usuario = usuarioService.getUsuarioByEmail(email).orElseThrow(MissingUserException::new);
+    public List<Pedido> getPedidosFromUsuario(String email) throws MissingUserException {
+        Usuario usuario = usuarioService.getUsuarioByEmail(email)
+                .orElseThrow(MissingUserException::new);
 
-        List<Pedido> pedidos = pedidoRepository.findByCliente(usuario.getId());
-
-        if (pedidos.isEmpty()) {
-            throw new MissingPedidoException();
-        }
-        return pedidos;
+        return pedidoRepository.findByCliente(usuario.getId());
     }
 
     @Override
